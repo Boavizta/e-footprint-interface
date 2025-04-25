464cfbab28df36da1dbcb70f703a1da801ecd461
Vincent Villet <vincent.villet@gmail.com>
2025-04-25 17:23:07 +0200

    [REFACTO] Remove font resize logic from side_panel_utils.js + move usage pattern time series positioning logic to custom.scss

--- theme/static/scripts/side_panel_utils.js
+++ theme/static/scripts/side_panel_utils.js
@@ -1,37 +1,6 @@
-let root = document.documentElement;
-
-let elementName = ['h6', 'h7', 'h8'];
-let elementSize = [
-    {"name": "min", "unit": "rem"},
-    {"name": "max", "unit": "rem"},
-    {"name": "ideal", "unit": "vw"}
-];
-let elementSizeValue = [];
-
-elementName.forEach((el) => {
-    elementSize.forEach((els) => {
-        elementSizeValue.push({
-            "id": `--${el}-${els.name}`,
-            "value": parseFloat(getComputedStyle(root).getPropertyValue(`--${el}-${els.name}`)),
-            "unit": els.unit
-        })
-    });
-})
-
-let factorToApplyOnFontSize = 0.2
-
-let widthLimit = 1200;
-
 function openSidePanel() {
     let sidePanel = document.getElementById("sidePanel");
     sidePanel.classList.remove("d-none");
-    let chartTimeseriesDiv = document.getElementById("chartTimeseries");
-    if (chartTimeseriesDiv) {
-        chartTimeseriesDiv.classList.add(window.innerWidth <= widthLimit ? "left-small" : "left-large");
-    }
-    elementSizeValue.forEach((el) => {
-        root.style.setProperty(el.id, `${el.value - factorToApplyOnFontSize}${el.unit}`);
-    })
     updateLines();
 }
 
@@ -44,8 +13,5 @@ function closeAndEmptySidePanel() {
     sidePanel.classList.add("d-none");
     sidePanel.innerHTML = "";
     closeTimeseriesChart();
-    elementSizeValue.forEach((el) => {
-        root.style.setProperty(el.id, `${el.value}${el.unit}`);
-    })
     updateLines();
 }
