cc0268a86d37ff4abd338d3cf384610fbee30cfe
Vincent Villet <vincent.villet@gmail.com>
2025-07-07 18:06:09 +0200

    [FIX] Close calculated attributes chart when opening side panel.

--- theme/static/scripts/side_panel_utils.js
+++ theme/static/scripts/side_panel_utils.js
@@ -18,6 +18,7 @@ function openSidePanel() {
         top: scrollTarget.offsetTop,
         behavior: "smooth"
     })
+    closeCalculatedAttributesChart();
 }
 
 function closeAndEmptySidePanel() {
