95d856263443fcd6d5dbb164031d613dc8b2ba4e
Simon Moliterni <simon.lamelliere@gmail.com>
2025-09-23 10:08:21 +0200

    Fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@
 
 # Run migrations
 poetry run python manage.py migrate
-poetry run python manage.py collectstatic --noinput --clear
 
 # Run NPM install
 npm install
+
+# Django Collect Static (assets)
+poetry run python manage.py collectstatic --noinput --clear
 
 # Then run the main container command (passed to us as arguments)
 exec "$@"
