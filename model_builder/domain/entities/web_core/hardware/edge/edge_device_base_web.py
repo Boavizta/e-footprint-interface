4d6f5234b67e0dc8e6e408a3059fb7a7ed6e94e3
VincentVillet <vincent.villet@gmail.com>
2025-12-10 11:33:22 +0100

    [REFACTO] Migrate EdgeComputerWeb and  EdgeDeviceBase forms.

--- model_builder/domain/entities/web_core/hardware/edge/edge_device_base_web.py
+++ model_builder/domain/entities/web_core/hardware/edge/edge_device_base_web.py
@@ -1,15 +1,9 @@
-from typing import TYPE_CHECKING
-
 from efootprint.builders.hardware.edge.edge_appliance import EdgeAppliance
 from efootprint.builders.hardware.edge.edge_computer import EdgeComputer
 from efootprint.core.hardware.edge.edge_device import EdgeDevice
 from efootprint.core.hardware.edge.edge_storage import EdgeStorage
 
 from model_builder.domain.entities.web_abstract_modeling_classes.modeling_object_web import ModelingObjectWeb
-from model_builder.domain.entities.web_core.hardware.hardware_utils import generate_object_with_storage_creation_context
-
-if TYPE_CHECKING:
-    from model_builder.domain.entities.web_core.model_web import ModelWeb
 
 
 class EdgeDeviceBaseWeb(ModelingObjectWeb):
@@ -18,6 +12,15 @@ class EdgeDeviceBaseWeb(ModelingObjectWeb):
     attributes_to_skip_in_forms = ["storage"]
     gets_deleted_if_unique_mod_obj_container_gets_deleted = False
 
+    # Declarative form configuration - used by FormContextBuilder in adapters layer
+    form_creation_config = {
+        'strategy': 'with_storage',
+        'object_type': 'EdgeDeviceBase',
+        'available_classes': [EdgeComputer, EdgeAppliance, EdgeDevice],
+        'storage_type': 'EdgeStorage',
+        'storage_classes': [EdgeStorage],
+    }
+
     @property
     def template_name(self):
         return "edge_device"
@@ -25,10 +28,3 @@ class EdgeDeviceBaseWeb(ModelingObjectWeb):
     @property
     def class_title_style(self):
         return "h6"
-
-    @classmethod
-    def generate_object_creation_context(
-    cls, model_web: "ModelWeb", efootprint_id_of_parent_to_link_to=None, object_type: str=None):
-        return generate_object_with_storage_creation_context(
-            model_web, "EdgeDeviceBase", [EdgeComputer, EdgeAppliance, EdgeDevice],
-            "EdgeStorage", [EdgeStorage])
